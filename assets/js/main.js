f2e76986a2d9c5a378ddc65910fe312895d84a9e
MrGaggio <steruggiu@gmail.com>
2021-10-15 12:01:11 +0100

    creata struttura base html per inserire l'immagine

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,13 @@
-alert("prova")
+/*
+Dati tre array contenenti:
+una lista ordinata di 5 immagini,
+una lista ordinata dei relativi 5 luoghi e
+una lista di 5 news, creare un carosello come nella foto allegata. Al click dell'utente sulle frecce verso l'alto o verso il basso, l'immagine attiva diventa visibile in formato grande a sinistra e nel suo angolo in basso a destra dovranno essere aggiunti i relativi
+titolo e
+testo. Allo stesso tempo nelle miniature l'immagine attiva dovrà apparire in evidenza rispetto alle altre.
+BONUS:
+Aggiungere il ciclo infinito del carosello. Ovvero se la miniatura attiva è la prima e l'utente clicca la freccia verso l'alto, la miniatura che deve attivarsi sarà l'ultima e viceversa per l'ultima miniatura se l'utente clicca la freccia verso il basso.
+
+
+
+//costruire un container dove visualizzare le foto e le miniature
